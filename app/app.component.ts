8000e5a7e08fb59cccd2e5b309b62fe25e8a123f
Abigail Rolling and Tyler Suderman <tcsuder@gmail.com>
2016-03-22 14:59:21 -0700

    initial file setup with keg object

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="jumbotron">
+      <h1>Tap Room Management</h1>
+    </div>
+    <div class="container">
+    </div>
+  `
+})
+
+export class AppComponent {
+
+}
